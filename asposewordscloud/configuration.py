03f663a07a39dcff81d21e02e58ec6cb9d2a1c45
jenkins <jenkins.aspose@gmail.com>
2018-08-03 07:27:07 +0000

    SDK regenerated by CI server

--- asposewordscloud/configuration.py
+++ asposewordscloud/configuration.py
@@ -276,5 +276,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 18.7.0\n"\
-               "SDK Package Version: 18.8".\
+               "SDK Package Version: 18.7".\
                format(env=sys.platform, pyversion=sys.version)
