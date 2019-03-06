40a10d34b02db87232825ec6f0cc3d0f3e0a1f43
yaroslaw.ekimov <yaroslawekimov@aspose.com>
2019-03-06 15:41:09 +0700

    update sdk version

--- asposewordscloud/api_client.py
+++ asposewordscloud/api_client.py
@@ -77,12 +77,12 @@ class ApiClient(object):
 
         self.pool = ThreadPool()
         self.rest_client = rest.RESTClientObject(configuration)
-        self.default_headers = {'x-aspose-client': 'python sdk', 'x-aspose-version': '18.9'}
+        self.default_headers = {'x-aspose-client': 'python sdk', 'x-aspose-version': '19.2'}
         if header_name is not None:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'python sdk 18.9'
+        self.user_agent = 'python sdk 19.2'
 
     def __del__(self):
         self.pool.close()
