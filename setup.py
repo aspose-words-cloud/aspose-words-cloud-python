2cea85ac0b8f67aa86b1ca38cc67914d86e60328
Evgeny Kuleshov <evgeny.kuleshov@aspose.com>
2019-08-06 15:28:59 +0300

    sdk version updated

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "aspose-words-cloud"
-VERSION = "18.9.0"
+VERSION = "19.8.0"
 # To install the library, run the following
 #
 # python setup.py install
