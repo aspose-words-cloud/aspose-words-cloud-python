b0be200d98d9f279b5d3761f8f84e761962e320a
Evgeny Kuleshov <evgeny.kuleshov@aspose.com>
2019-11-11 01:53:17 +0300

    Package version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "aspose-words-cloud"
-VERSION = "19.9.2"
+VERSION = "19.10.0"
 # To install the library, run the following
 #
 # python setup.py install
