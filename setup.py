f7fe3fe6f16ac12c1b32ab646ab9c6aafba35e88
yaroslaw.ekimov <yaroslawekimov@aspose.com>
2020-01-22 19:26:17 +0700

    update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "aspose-words-cloud"
-VERSION = "19.12.0"
+VERSION = "20.1.0"
 # To install the library, run the following
 #
 # python setup.py install
