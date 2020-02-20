fe9615b5f0cd98d3aa8ce78b357d40247c729a81
wordscloud <aspose.cloud@aspose.com>
2020-02-20 08:05:52 -0800

    SDK regenerated by CI server [ci skip]

--- asposewordscloud/models/paragraph_format_response.py
+++ asposewordscloud/models/paragraph_format_response.py
@@ -30,7 +30,7 @@ import six
 
 
 class ParagraphFormatResponse(object):
-    """This response should be returned by the service when handling: GET http://api.aspose.com/v4.0/words/Test.doc/paragraphs/{0}/format.
+    """This response should be returned by the service when handling: GET https://api.aspose.cloud/v4.0/words/Test.doc/paragraphs/{0}/format.
     """
 
     """
