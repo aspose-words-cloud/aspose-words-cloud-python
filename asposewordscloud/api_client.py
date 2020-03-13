50b65c5c1d05b21c07613f8b68985842c4a2548e
wordscloud <aspose.cloud@aspose.com>
2020-03-12 23:01:57 -0700

    SDK regenerated by CI server [ci skip]

--- asposewordscloud/api_client.py
+++ asposewordscloud/api_client.py
@@ -77,12 +77,12 @@ class ApiClient(object):
 
         self.pool = ThreadPool()
         self.rest_client = rest.RESTClientObject(configuration)
-        self.default_headers = {'x-aspose-client': 'python sdk', 'x-aspose-version': '19.12'}
+        self.default_headers = {'x-aspose-client': 'python sdk', 'x-aspose-version': '20.3'}
         if header_name is not None:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'python sdk 19.12'
+        self.user_agent = 'python sdk 20.3'
 
     def __del__(self):
         self.pool.close()
@@ -450,13 +450,12 @@ class ApiClient(object):
                     continue
                 file_names = v if type(v) is list else [v]
                 for n in file_names:
-                    with open(n, 'rb') as f:
-                        filename = os.path.basename(f.name)
-                        filedata = f.read()
-                        mimetype = (mimetypes.guess_type(filename)[0] or
-                                    'application/octet-stream')
-                        params.append(
-                            tuple([k, tuple([filename, filedata, mimetype])]))
+                    filename = os.path.basename(n.name)
+                    filedata = n.read()
+                    mimetype = (mimetypes.guess_type(filename)[0] or
+                                'application/octet-stream')
+                    params.append(
+                        tuple([k, tuple([filename, filedata, mimetype])]))
 
         return params
 
