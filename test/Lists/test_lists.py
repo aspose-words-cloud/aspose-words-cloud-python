96a9d9778505372684c9f1683b1a1b9a1f88698a
ivan.kishchenko <ivan.kishchenko@aspose.com>
2020-04-09 18:35:23 +0300

    Fix tests

--- test/Lists/test_lists.py
+++ test/Lists/test_lists.py
@@ -37,32 +37,32 @@ class TestLists(BaseTestContext):
     def test_get_lists(self):
         remote_name = 'TestGetLists.doc'
         self.upload_file(os.path.join(self.remote_test_folder, remote_name), open(os.path.join(self.local_test_folder, self.test_folder, self.local_name), 'rb'))
-        request = asposewordscloud.models.requests.GetListsRequest(remote_name)
+        request = asposewordscloud.models.requests.GetListsRequest(remote_name, self.remote_test_folder)
         _ = self.words_api.get_lists(request)
 
     def test_get_list(self):
         remote_name = 'TestGetList.doc'
         self.upload_file(os.path.join(self.remote_test_folder, remote_name), open(os.path.join(self.local_test_folder, self.test_folder, self.local_name), 'rb'))
-        request = asposewordscloud.models.requests.GetListRequest(remote_name, 1)
+        request = asposewordscloud.models.requests.GetListRequest(remote_name, 1, self.remote_test_folder)
         _ = self.words_api.get_list(request)
 
     def test_update_list(self):
         remote_name = 'TestUpdateList.doc'
         self.upload_file(os.path.join(self.remote_test_folder, remote_name), open(os.path.join(self.local_test_folder, self.test_folder, self.local_name), 'rb'))
         data = asposewordscloud.models.ListUpdate(True)
-        request = asposewordscloud.models.requests.UpdateListRequest(remote_name, data, 1)
+        request = asposewordscloud.models.requests.UpdateListRequest(remote_name, data, 1, self.remote_test_folder)
         _ = self.words_api.update_list(request)
 
     def test_update_list_level(self):
         remote_name = 'TestUpdateListLevel.doc'
         self.upload_file(os.path.join(self.remote_test_folder, remote_name), open(os.path.join(self.local_test_folder, self.test_folder, self.local_name), 'rb'))
         data = asposewordscloud.models.ListLevelUpdate(alignment="Right")
-        request = asposewordscloud.models.requests.UpdateListLevelRequest(remote_name, data, 1, 1)
+        request = asposewordscloud.models.requests.UpdateListLevelRequest(remote_name, data, 1, 1, self.remote_test_folder)
         _ = self.words_api.update_list_level(request)
 
     def test_insert_list(self):
         remote_name = 'TestInsertList.doc'
         self.upload_file(os.path.join(self.remote_test_folder, remote_name), open(os.path.join(self.local_test_folder, self.test_folder, self.local_name), 'rb'))
         data = asposewordscloud.models.ListInsert("OutlineLegal")
-        request = asposewordscloud.models.requests.InsertListRequest(remote_name, data)
+        request = asposewordscloud.models.requests.InsertListRequest(remote_name, data, self.remote_test_folder)
         _ = self.words_api.insert_list(request)
