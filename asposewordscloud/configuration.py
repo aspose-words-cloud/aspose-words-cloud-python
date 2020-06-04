c506959d4c4e2b313d90cab01509966826f4397f
wordscloud <aspose.cloud@aspose.com>
2020-06-04 06:06:45 -0700

    SDK regenerated by CI server [ci skip]

--- asposewordscloud/configuration.py
+++ asposewordscloud/configuration.py
@@ -259,6 +259,6 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 20.5.0\n"\
+               "Version of the API: 20.6.0\n"\
                "SDK Package Version: 18.9.0".\
                format(env=sys.platform, pyversion=sys.version)
