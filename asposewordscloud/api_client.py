c2b5aad2d24c467ba6a3d280047d63e007c99d3d
evgeny.ivanov <evgeny.ivanov>
2020-07-03 06:11:09 -0500

    SDK regenerated by CI server [ci skip]

--- asposewordscloud/api_client.py
+++ asposewordscloud/api_client.py
@@ -77,12 +77,12 @@ class ApiClient(object):
 
         self.pool = ThreadPool()
         self.rest_client = rest.RESTClientObject(configuration)
-        self.default_headers = {'x-aspose-client': 'python sdk', 'x-aspose-version': '20.6'}
+        self.default_headers = {'x-aspose-client': 'python sdk', 'x-aspose-version': '20.7'}
         if header_name is not None:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'python sdk 20.6'
+        self.user_agent = 'python sdk 20.7'
 
     def __del__(self):
         self.pool.close()
