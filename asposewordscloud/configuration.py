c422e6d0015287a7bdfa0bc0f7a2c06325ed2359
evgeny.ivanov <evgeny.ivanov>
2020-12-27 22:42:33 -0600

    SDK regenerated by CI server [ci skip]

--- asposewordscloud/configuration.py
+++ asposewordscloud/configuration.py
@@ -258,6 +258,6 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 20.11\n"\
-               "SDK Package Version: 20.11".\
+               "Version of the API: 20.12\n"\
+               "SDK Package Version: 20.12".\
                format(env=sys.platform, pyversion=sys.version)
