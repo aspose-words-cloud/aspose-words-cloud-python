3d9460315feeff67ec8d47eded951b3f74846e6b
evgeny.ivanov <evgeny.ivanov>
2021-02-11 00:06:28 -0600

    SDK regenerated by CI server [ci skip]

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         'Topic :: Office/Business :: Office Suites',
 		'Topic :: Software Development :: Libraries',
 		'License :: OSI Approved :: MIT License',
-		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
