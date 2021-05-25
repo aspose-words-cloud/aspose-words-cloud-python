d4f36c4bb7550f6d1f3d9ef5604a0d5a58c3f8d2
evgeny.ivanov <evgeny.ivanov>
2021-05-25 00:15:56 -0500

    SDK regenerated by CI server [ci skip]

--- asposewordscloud/configuration.py
+++ asposewordscloud/configuration.py
@@ -258,6 +258,6 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 21.5.1\n"\
-               "SDK Package Version: 21.5.1".\
+               "Version of the API: 21.5\n"\
+               "SDK Package Version: 21.5".\
                format(env=sys.platform, pyversion=sys.version)
