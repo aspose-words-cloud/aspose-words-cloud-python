5680248f8505f8ea89bf5ac827bf8f9822e09ec9
evgeny.ivanov <evgeny.ivanov>
2021-07-20 02:12:46 -0500

    SDK regenerated by CI server [ci skip]

--- Examples/UpdateBookmark.py
+++ Examples/UpdateBookmark.py
@@ -2,5 +2,5 @@ words_api = WordsApi(client_id = '####-####-####-####-####', client_secret = '##
 remote_file_name= 'Sample.docx'
 bookmark_name= 'aspose'
 
-update_bookmark = asposewordscloud.models.requests.UpdateBookmarkRequest(name = remote_file_name,bookmark_name = bookmark_name,bookmark_data = request_bookmark_data,dest_file_name = self.remote_test_out + '/' + remote_file_name)
+update_bookmark = asposewordscloud.models.requests.UpdateBookmarkRequest(name = remote_file_name,bookmark_name = bookmark_name,bookmark_data = test_bookmark_data)
 words_api.update_bookmark(update_bookmark)
