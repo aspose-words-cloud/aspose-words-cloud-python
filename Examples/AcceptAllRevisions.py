faf96711b8de2ee8a0b22f59e1ab4472055f360e
evgeny.ivanov <evgeny.ivanov>
2021-08-19 05:36:29 -0500

    SDK regenerated by CI server [ci skip]

--- Examples/AcceptAllRevisions.py
+++ Examples/AcceptAllRevisions.py
@@ -4,12 +4,11 @@ import asposewordscloud.models.requests
 from asposewordscloud.rest import ApiException
 from shutil import copyfile
 
-documents_dir = '...'
 words_api = WordsApi(client_id = '####-####-####-####-####', client_secret = '##################') 
 file_name = 'test_doc.docx'
 
 # Upload original document to cloud storage.
-my_var1 = open(os.path.join(documents_dir, file_name), 'rb')
+my_var1 = open(file_name, 'rb')
 my_var2 = file_name
 upload_file_request = asposewordscloud.models.requests.UploadFileRequest(file_content = my_var1,path = my_var2)
 words_api.upload_file(upload_file_request)
