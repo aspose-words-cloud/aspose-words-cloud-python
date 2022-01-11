f33bb1b4183db2db66488d8557fa238c01ec02db
jenkins <jenkins.aspose@gmail.com>
2022-01-11 03:18:22 -0600

    SDK regenerated by CI server [ci skip]

--- asposewordscloud/configuration.py
+++ asposewordscloud/configuration.py
@@ -258,6 +258,6 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 21.12\n"\
-               "SDK Package Version: 21.12".\
+               "Version of the API: 22.1\n"\
+               "SDK Package Version: 22.1".\
                format(env=sys.platform, pyversion=sys.version)
