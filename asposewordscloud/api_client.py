c51fffb8e8a4ce66e920adeda3c88d33d21d8e17
jenkins <jenkins.aspose@gmail.com>
2022-04-18 22:37:41 -0500

    SDK regenerated by CI server [ci skip]

--- asposewordscloud/api_client.py
+++ asposewordscloud/api_client.py
@@ -83,12 +83,12 @@ class ApiClient(object):
 
         self.pool = None
         self.rest_client = rest.RESTClientObject(configuration)
-        self.default_headers = {'x-aspose-client': 'python sdk', 'x-aspose-version': '22.4'}
+        self.default_headers = {'x-aspose-client': 'python sdk', 'x-aspose-version': '1.0'}
         if header_name is not None:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'python sdk 22.4'
+        self.user_agent = 'python sdk 1.0'
 
     def __del__(self):
         if not self.pool is None:
--- asposewordscloud/configuration.py
+++ asposewordscloud/configuration.py
@@ -262,6 +262,6 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 22.4\n"\
-               "SDK Package Version: 22.4".\
+               "Version of the API: 1.0\n"\
+               "SDK Package Version: 1.0".\
                format(env=sys.platform, pyversion=sys.version)
