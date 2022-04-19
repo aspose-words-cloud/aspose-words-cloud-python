67578018540660856fefb264234f11c7b4a31665
jenkins <jenkins.aspose@gmail.com>
2022-04-19 01:23:45 -0500

    SDK regenerated by CI server [ci skip]

--- asposewordscloud/api_client.py
+++ asposewordscloud/api_client.py
@@ -83,12 +83,12 @@ class ApiClient(object):
 
         self.pool = None
         self.rest_client = rest.RESTClientObject(configuration)
-        self.default_headers = {'x-aspose-client': 'python sdk', 'x-aspose-version': '1.0'}
+        self.default_headers = {'x-aspose-client': 'python sdk', 'x-aspose-version': '22.4'}
         if header_name is not None:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'python sdk 1.0'
+        self.user_agent = 'python sdk 22.4'
 
     def __del__(self):
         if not self.pool is None:
