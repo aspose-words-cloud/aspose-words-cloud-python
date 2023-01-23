9ef8efbe8ff1aafc44b838765f13ffc773153b00
jenkins <jenkins.aspose@gmail.com>
2023-01-23 10:33:02 -0600

    SDK regenerated by CI server [ci skip]

--- test/api/mail_merge/test_execute_template_with_field_options.py
+++ test/api/mail_merge/test_execute_template_with_field_options.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------------------------------------------
 # <copyright company="Aspose" file="test_execute_template_with_field_options.py">
-#   Copyright (c) 2022 Aspose.Words for Cloud
+#   Copyright (c) 2023 Aspose.Words for Cloud
 # </copyright>
 # <summary>
 #   Permission is hereby granted, free of charge, to any person obtaining a copy
