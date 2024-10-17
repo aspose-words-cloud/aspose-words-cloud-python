b5d06c38238d8b292439fbb59d91cc2733990456
jenkins <jenkins.aspose@gmail.com>
2024-10-17 08:00:04 -0500

    SDK regenerated by CI server [ci skip]

--- asposewordscloud/apis/words_api.py
+++ asposewordscloud/apis/words_api.py
@@ -7454,6 +7454,163 @@ class WordsApi(Encryptor, object):
             collection_formats=http_params['collection_formats']))
 
 
+    def get_all_revisions(self, request, **kwargs):  # noqa: E501
+        """Get all information about revisions.  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass is_async=True
+
+        :param is_async bool
+        :param name str : The filename of the input document. (required)
+        :param folder str : Original document folder.
+        :param storage str : Original document storage.
+        :param load_encoding str : Encoding that will be used to load an HTML (or TXT) document if the encoding is not specified in HTML.
+        :param password str : Password of protected Word document. Use the parameter to pass a password via SDK. SDK encrypts it automatically. We don't recommend to use the parameter to pass a plain password for direct call of API.
+        :param encrypted_password str : Password of protected Word document. Use the parameter to pass an encrypted password for direct calls of API. See SDK code for encyption details.
+        :param open_type_support bool : The value indicates whether OpenType support is on.
+        :return: RevisionsResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        try:
+            if kwargs.get('is_async'):
+                return self.get_all_revisions_with_http_info(request, **kwargs)  # noqa: E501
+            data = self.get_all_revisions_with_http_info(request, **kwargs)  # noqa: E501
+            return data
+        except ApiException as e:
+            if e.status == 401:
+                self.api_client.request_token()
+                if kwargs.get('is_async'):
+                    return self.get_all_revisions_with_http_info(request, **kwargs)  # noqa: E501
+            data = self.get_all_revisions_with_http_info(request, **kwargs)  # noqa: E501
+            return data
+        
+    def get_all_revisions_with_http_info(self, request, **kwargs):  # noqa: E501
+        """Get all information about revisions.  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass is_async=True
+
+        :param is_async bool
+        :param request GetAllRevisionsRequest object with parameters
+        :return: RevisionsResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        params = locals()
+        params['is_async'] = ''
+        params['_preload_content'] = True
+        params['_request_timeout'] = self.timeout
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_all_revisions" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        http_params = request.create_http_request(self.api_client, self)
+
+        # Authentication setting
+        auth_settings = ['JWT']  # noqa: E501
+
+        self.api_client.handle_password(http_params, self)
+
+        return request.deserialize_response(self.api_client, self.api_client.call_api(
+            http_params['path'],
+            http_params['method'],
+            http_params['query_params'],
+            http_params['header_params'],
+            body=None,
+            post_params=http_params['form_params'],
+            response_type=http_params['response_type'],  # noqa: E501
+            auth_settings=auth_settings,
+            is_async=params.get('is_async'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=http_params['collection_formats']))
+
+
+    def get_all_revisions_online(self, request, **kwargs):  # noqa: E501
+        """Get all information about revisions.  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass is_async=True
+
+        :param is_async bool
+        :param document file : The document. (required)
+        :param load_encoding str : Encoding that will be used to load an HTML (or TXT) document if the encoding is not specified in HTML.
+        :param password str : Password of protected Word document. Use the parameter to pass a password via SDK. SDK encrypts it automatically. We don't recommend to use the parameter to pass a plain password for direct call of API.
+        :param encrypted_password str : Password of protected Word document. Use the parameter to pass an encrypted password for direct calls of API. See SDK code for encyption details.
+        :param open_type_support bool : The value indicates whether OpenType support is on.
+        :return: RevisionsResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        try:
+            if kwargs.get('is_async'):
+                return self.get_all_revisions_online_with_http_info(request, **kwargs)  # noqa: E501
+            data = self.get_all_revisions_online_with_http_info(request, **kwargs)  # noqa: E501
+            return data
+        except ApiException as e:
+            if e.status == 401:
+                self.api_client.request_token()
+                if kwargs.get('is_async'):
+                    return self.get_all_revisions_online_with_http_info(request, **kwargs)  # noqa: E501
+            data = self.get_all_revisions_online_with_http_info(request, **kwargs)  # noqa: E501
+            return data
+        
+    def get_all_revisions_online_with_http_info(self, request, **kwargs):  # noqa: E501
+        """Get all information about revisions.  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass is_async=True
+
+        :param is_async bool
+        :param request GetAllRevisionsOnlineRequest object with parameters
+        :return: RevisionsResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        params = locals()
+        params['is_async'] = ''
+        params['_preload_content'] = True
+        params['_request_timeout'] = self.timeout
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_all_revisions_online" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        http_params = request.create_http_request(self.api_client, self)
+
+        # HTTP header `Accept`
+        http_params['header_params']['Accept'] = self.api_client.select_header_accept(
+            ['application/xml', 'application/json'])  # noqa: E501
+        # Authentication setting
+        auth_settings = ['JWT']  # noqa: E501
+
+        self.api_client.handle_password(http_params, self)
+
+        return request.deserialize_response(self.api_client, self.api_client.call_api(
+            http_params['path'],
+            http_params['method'],
+            http_params['query_params'],
+            http_params['header_params'],
+            body=None,
+            post_params=http_params['form_params'],
+            response_type=http_params['response_type'],  # noqa: E501
+            auth_settings=auth_settings,
+            is_async=params.get('is_async'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=http_params['collection_formats']))
+
+
     def get_available_fonts(self, request, **kwargs):  # noqa: E501
         """Reads available fonts from the document.  # noqa: E501
 
