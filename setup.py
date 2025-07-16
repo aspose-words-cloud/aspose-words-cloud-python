e74ae8eb7b20258d2560070bce74036ae7730a0d
jenkins <jenkins.aspose@gmail.com>
2025-07-16 00:52:11 -0500

    SDK regenerated by CI server [ci skip]

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 
 from setuptools import setup, find_packages  # noqa: H301
 
-NAME = "aspose-words-cloud"
+NAME = "aspose_words_cloud"
 VERSION = "25.7.0"
 # To install the library, run the following
 #
