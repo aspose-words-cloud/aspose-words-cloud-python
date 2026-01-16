aac347b21504097901bcc66409ecbb9481d39dfd
jenkins <jenkins.aspose@gmail.com>
2026-01-16 09:55:28 -0600

    SDK regenerated by CI server [ci skip]

--- test/test_doc_with_password.py
+++ test/test_doc_with_password.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # -----------------------------------------------------------------------------------
 # <copyright company="Aspose" file="test_doc_with_password.py">
-#   Copyright (c) 2025 Aspose.Words for Cloud
+#   Copyright (c) 2026 Aspose.Words for Cloud
 # </copyright>
 # <summary>
 #   Permission is hereby granted, free of charge, to any person obtaining a copy
